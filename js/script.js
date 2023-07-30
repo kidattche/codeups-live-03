254be63817a7339f73c8332216a23682faa65243
kidattche <kidattche200577@gmail.com>
2023-07-31 06:39:19 +0900

    トップページPC/SP完了

--- js/script.js
+++ js/script.js
@@ -40,14 +40,29 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
-  $(document).on('click', 'a[href*="#"]', function () {
+$(document).on('click', 'a[href*="#"]', function (event) {
+    event.preventDefault();
+    
     let time = 400;
     let header = $('header').innerHeight();
     let target = $(this.hash);
     if (!target.length) return;
     let targetY = target.offset().top - header;
     $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
+    // return false;
+ });
+})
 
-});
+    var swiper = new Swiper(".js-works-swiper", {
+      pagination: {
+        el: ".js-works-paginations",
+        type: 'bullets',
+        clickable: true,
+        autoplay: {
+        delay: 3000,
+        }
+        
+        },
+        loop: true,
+    });
+  
